b3a7c793419b70e5424da287d31c45c795c517f4
april-gomez <123357751+april-gomez@users.noreply.github.com>
2023-01-23 16:16:36 +0800

    Add files via upload

---
+++ addNumbers/app.js
@@ -0,0 +1,5 @@
+var add = require('./add.js');
+
+var addNum = new add(21, 04);
+
+console.log(addNum.sum());
---
+++ myGreetings/callGreet.js
@@ -0,0 +1,5 @@
+var greet = require('./greet.js');
+
+var name = new greet('John', 'Jane');
+
+console.log(name.greeting());
