fcc9f8111c3d74633f38c5cf5721dbed8a91f3ed
april-gomez <123357751+april-gomez@users.noreply.github.com>
2023-02-13 15:21:27 +0800

    1 file modified

--- Midterms/firstexpress/firstapp.js
+++ Midterms/firstexpress/firstapp.js
@@ -1,7 +1,8 @@
 var express = require('express');
+
 var app = express();
 
-app.get('/', function (request, respons) {
+app.get('/', function (request, response) {
   response.send('Hello World!');
 });
 
